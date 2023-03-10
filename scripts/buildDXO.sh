9d8a2217e2914c104e6f02d7e951af8db9d1397e
Bryan J Rodriguez <bryan.j.rodriguez@intel.com>
2023-03-09 20:34:04 -0700

    debugging

--- scripts/buildDXO.sh
+++ scripts/buildDXO.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # we want to have some checks done for undefined variables
 set -u
 
@@ -10,17 +9,17 @@ if [ -z "${TOKEN:-}" ]; then
 fi
 
 docker run -i --rm --privileged ${DOCKER_RUN_ARGS} -e TOKEN="${TOKEN}" -v /var/run:/var/run docker:stable sh -c "\
-  apk update && apk add --no-cache \ 
+apk update && apk add --no-cache \
     bash \
     coreutils \
     git \
     ncurses \
     pv \
     tar \
-    wget && \
+    wget &&
 
-  mkdir /work && \
-  cd work && \
-  git clone --depth=1 https://${TOKEN}:${TOKEN}@github.com/brod-intel/dxo.git dxo && \
-  cd dxo && \
-  docker build ${DOCKER_BUILD_ARGS} -t ace/dxo:1.0 -f build/Dockerfile ."
+mkdir /work && \
+cd work && \
+git clone --depth=1 https://${TOKEN}:${TOKEN}@github.com/brod-intel/dxo.git dxo && \
+cd dxo && \
+docker build ${DOCKER_BUILD_ARGS} -t ace/dxo:1.0 -f build/Dockerfile ."
